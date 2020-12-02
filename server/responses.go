1c9198582f58831d46891ed507bece21d868cf55
Kai Johnson <kai.johnson@eagleview.com>
2020-12-02 15:24:38 -0800

    updates statuses

--- server/responses.go
+++ server/responses.go
@@ -1,7 +1,12 @@
 package server
 
 const (
+	// HealthStatusOK the state for a operational service.
 	HealthStatusOK = "OK"
+	// HealthStatusWarning the state when a service is operational, however, with some warning condition.
+	HealthStatusWarning = "WARNING"
+	// HealthStatusBad the state when something dire is occuring in the service.
+	HealthStatusBad = "BAD"
 )
 
 // HealthStatusResponse the response for a health request.
